166f457be9e075276f777ec332d3d6cbf1f9c8d9
Anandhu-CHN <anandhu27122005@gmail.com>
2026-06-28 01:09:27 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const CACHE_NAME = 'gate-v1';
 // We include the relative path './' to ensure it points to your repo folder
-const ASSETS = ['./', './index.html', './manifest.json']; 
+const ASSETS = ['./', './index.html', './manifest.json','./icons.png']; 
 
 self.addEventListener('install', (e) => {
     e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
