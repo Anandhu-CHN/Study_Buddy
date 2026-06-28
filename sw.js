1b1d6bdce195efc2699110fa9141c72f7ee51594
Anandhu-CHN <anandhu27122005@gmail.com>
2026-06-29 00:48:44 +0530

    Change icons file extension from .png to .jpg

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const CACHE_NAME = 'gate-v1';
 // We include the relative path './' to ensure it points to your repo folder
-const ASSETS = ['./', './index.html', './manifest.json','./icons.png']; 
+const ASSETS = ['./', './index.html', './manifest.json','./icons.jpg']; 
 
 self.addEventListener('install', (e) => {
     e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
